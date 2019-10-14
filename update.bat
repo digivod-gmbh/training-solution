ac6b82d0076a75ab847524c155a9dacf922cb74d
digivod-gmbh <entwicklung@digivod.de>
2019-10-14 17:10:44 +0200

    Updated update.bat

--- update.bat
+++ update.bat
@@ -3,5 +3,22 @@
 REM go to directory of update.bat (needed for admin mode)
 cd %~dp0
 
+git status
+echo.
+echo.
+
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Updating will reset all local changes. Do you want to continue (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+git checkout -- .
 git pull
-call install.bat
+
+REM update TrainingSolution
+pip install -e .
+
+:END
+endlocal
+
+pause
